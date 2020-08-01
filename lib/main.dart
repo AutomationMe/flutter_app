1ffd5cf0cc0761303ec59d56e39e32c7e8726fdf
autocurry <sandeepsekhar555@gmail.com>
2020-08-01 18:41:09 +0530

    Stateless app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,56 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+//stateless app
+void main(){
+runApp(MyApp());
+}
 
-void main() => runApp(
-  Center(
-    child: Text('Welcome to First Flutter App',textDirection:TextDirection.ltr),
-  )
+class MyApp extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context){
+  return MaterialApp(
+  title : 'Flutter App',
+  debugShowCheckedModeBanner: false,
+  theme: ThemeData(
+    primaryColor: Colors.lightGreen,
+    accentColor: Colors.lightBlue,
+  ),
+
+  
+  home:Scaffold(
+    appBar: AppBar(
+      title: Text('Flutter'),
+      //backgroundColor: Colors.black,
+    ),
+
+body: Center(
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Text('Welcome to the first flutter app'),
+      Text('This has been created for learning purpose!'),
+      RaisedButton(
+        onPressed:(){},
+        child: Text('SignUp'),
+        color: Colors.blue,
+        splashColor: Colors.white,
+        )
+    ],
+  ),
+),
+
+
+
+    floatingActionButton: FloatingActionButton(
+      onPressed:() {},
+      child: Icon( 
+      Icons.add_a_photo,
+      color:Colors.black,
+      ),
+  ),
+
+
+),
 );
+}
+}
